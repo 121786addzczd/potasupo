005e234be921933dc2fcce5fd121ee53aed9d8ea
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2020-12-01 18:01:42 +0900

    ユーザーログイン機能実装
    Google APIを使ってGoogleアカウントでログインできるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'reviews/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  root to: 'reviews#index'
+  resources :reviews
+
+  resources :users, only: [:show, :edit, :update]
 end
