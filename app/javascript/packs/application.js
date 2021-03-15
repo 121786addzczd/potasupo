418844e62cbf110fe369ca3992d7ca89f971d988
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-15 12:41:05 +0900

    spotの投稿画像プレビュー機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ require('./open_menu')
 require('./footer')
 require('./passwordDisplay')
 require('./flashMessageAutoClose')
-
+require('./imagePreview')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
