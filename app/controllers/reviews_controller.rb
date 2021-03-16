5011840ddd8ac8545604e68c36550ff67ca955b5
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-16 13:22:18 +0900

    レビュー投稿機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def index
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to spot_reviews_path(@review.spot)
+    else
+      flash[:alert] = "レビューの投稿に失敗しました"
+      @spot = Spot.find(params[:spot_id])
+      render "spots/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:spot_id, :score, :content)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :spot
+
+  validates :score, presence: true
+  validates :user_id, uniqueness: { scope: :spot_id }
 end
